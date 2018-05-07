c2f1749ecf321317a2f603acda062e37dd39a15b
Felipe Diogo <felipe.diogo@ilegra.com>
2018-05-06 21:05:04 -0300

    pelo menos o config de dat vai ficar no properties

--- src/main/java/br/com/felipediogo/camel/routers/FileRouter.java
+++ src/main/java/br/com/felipediogo/camel/routers/FileRouter.java
@@ -1,12 +1,14 @@
 package br.com.felipediogo.camel.routers;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileRouter extends RouteBuilder {
 
-    private static final String FILE_EXTENSION = ".dat";
+    @Value("${file_extension}")
+    private String fileExtension;
 
     @Override
     public void configure() {
@@ -20,7 +22,8 @@ public class FileRouter extends RouteBuilder {
         return new StringBuilder()
                 .append("file://")
                 .append(homePath())
-                .append(String.format("/data/in?charset=UTF-8&preMove=staging&moveFailed=../.failed&move=../.done&include=.*%s$", FILE_EXTENSION)).toString();
+                .append(String.format("/data/in?charset=UTF-8&preMove=staging&moveFailed=../.failed&move=../.done&include=.*%s$",
+                        fileExtension)).toString();
     }
 
     private String fileOutRoute() {
